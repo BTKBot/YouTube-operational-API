63486c928d338c2f16fa38b25934a1cffe66e895
BTKBot <88508980+BTKBot@users.noreply.github.com>
2025-08-31 00:39:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
-        <title>YouTube operational API BTKBot five</title>
+        <title>YouTube operational API BTKBot six</title>
         <style>
             body {
                 max-width: 73%;
